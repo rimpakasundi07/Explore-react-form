c1159138bfa3309f108eba40aac531b9a8a9a8ff
rimpa kasundi <rimpakasundi@gmail.com>
2025-10-08 21:03:53 +0530

    Convert form to use React action with FormData for name and email handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import FormAction from "./components/FormAction/FormAction";
 import SimpleForm from "./components/SimpleForm/SimpleForm";
 
 function App() {
   return (
     <>
       <h1>Explore React From</h1>
-      <SimpleForm></SimpleForm>
+      {/* <SimpleForm></SimpleForm> */}
+      <FormAction></FormAction>
     </>
   );
 }
