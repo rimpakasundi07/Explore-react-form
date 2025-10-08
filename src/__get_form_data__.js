04d66830115a6876488634714669c2df993412fb
rimpa kasundi <rimpakasundi@gmail.com>
2025-10-08 22:43:44 +0530

    Implement uncontrolled form inputs using useRef for email and password

--- src/__get_form_data__.js
+++ src/__get_form_data__.js
@@ -1,4 +1,17 @@
 /**
  * 1.  e.target.[name of the input field].value
  * 2.  use form action and formdata in the action handler. formData.get('name of the input field')
+ * 3. controlled component.one par each field use state on charge of the filed. Useful to dynamically handle error
+ *
+ * OR  3. ///
+ *  handle all controlled field on one state object
+ *
+ *  const [formData , setFormData ] = useState({
+ *  name: '' ,
+ *  password: '' ,
+ *  phone: '' ,
+ *  })
+ *
+ * 4. uncontrolled using useRef
+ *
  */
--- src/components/ControlledField/ControlledField.jsx
+++ src/components/ControlledField/ControlledField.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 
 const ControlledField = () => {
-  const [password, setPassword] = useState("");
+  const [password, setPassword] = useState("secret");
+  const [error, setError] = useState("");
 
   const handleSubmit = (e) => {
     e.preventdefault();
@@ -11,23 +12,21 @@ const ControlledField = () => {
   const handlePasswordOnChange = (e) => {
     console.log(e.target.value);
     setPassword(e.target.value);
+    if (password.length < 6) {
+      setError("Password must be 6 character or longer.");
+    } else {
+      setError("");
+    }
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          name="email"
-          id=""
-          placeholder="Enter Email"
-          required
-        />
+        <input type="email" name="email" placeholder="Enter Email" required />
         <br></br>
         <input
           type="password"
           name="password"
-          id=""
           placeholder="password"
           onChange={handlePasswordOnChange}
           defaultValue={password}
@@ -36,6 +35,9 @@ const ControlledField = () => {
         <br></br>
         <input type="submit" value="submit" />
       </form>
+      <p style={{ color: "red" }}>
+        <small>{error}</small>
+      </p>
     </div>
   );
 };
